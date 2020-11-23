026da6c076158b1904e4398215c1fd16b058e67e
kalemi <kaveh_alemi@sfu.ca>
2020-11-22 17:40:27 -0800

    Update Trending_Video_Views_Analysis.py

--- Trending_Video_Views_Analysis.py
+++ Trending_Video_Views_Analysis.py
@@ -29,6 +29,7 @@ from scipy import stats
 from time import sleep
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 import sys
+import datetime as dt
 
 
 ########################### FUNCTIONS ####################################
@@ -61,6 +62,11 @@ def get_merged_data_2018(vids_dir, cats_dir):
 
     # Import the compressed csv file
     vids_df = pd.read_csv(vids_dir)
+
+    # Subset to only include first 4 months of 2018
+    vids_df['trending_date'] =  pd.to_datetime(vids_df['trending_date'], format='%y.%d.%m')
+    vids_df = vids_df[vids_df['trending_date'].dt.strftime('%Y') == '2018']
+    vids_df = vids_df[vids_df['trending_date'].dt.strftime('%m') < '05'].reset_index()
 
     # Load category file
     cats_df = convert_json_cats_to_pandas_2018(cats_dir)
@@ -156,6 +162,11 @@ def get_view_data_2018(vids_dir, cats_dir):
     # Import the video trending data
     vids_df = pd.read_csv(vids_dir)
 
+    # Subset data to only include first 4 months of 2018
+    vids_df['trending_date'] =  pd.to_datetime(vids_df['trending_date'], format='%y.%d.%m')
+    vids_df = vids_df[vids_df['trending_date'].dt.strftime('%Y') == '2018']
+    vids_df = vids_df[vids_df['trending_date'].dt.strftime('%m') < '05'].reset_index()    
+
     # Load category file
     cats_df = convert_json_cats_to_pandas_2018(cats_dir)
     cats_df['category_id'] = cats_df['category_id'].astype(int)
@@ -186,6 +197,60 @@ def get_view_data_2020(vids_dir, cats_dir):
     return(ret)
 
 
+# Plot hustogram of views per category of inputted dataframe
+def plot_views_hist(data_per_year, country, year, transformed):
+
+    # Create plot axis
+    fig, ax = plt.subplots(figsize=(10,7))
+    for label, df in data_per_year.groupby('cat_name'):
+        df.views.plot(kind="hist", ax=ax, label=label)
+        
+    plt.legend()
+
+    # Create the title of the barplot
+    title = str(country) + ' - Histogram of Views per category (' + str(year) + ')'
+    if(transformed):
+        title += '(Transformed'
+    plt.title(title)
+
+    # Save file to local workiing directory
+    file_title = title + '.png'
+    plt.savefig(file_title, dpi=100)
+
+
+# Perform normality test on the views of each category of the inputted dataframe
+def normality_test_views(data_per_year, country, year):
+    for label, df in data_per_year.groupby('cat_name'):
+        cat_name = df.cat_name.unique()[0]
+        print(str(country) + ' - Normality test P-Value for ' + str(cat_name) + ' (' + str(year) + ')')
+        print(stats.normaltest(df.views)[1])
+        print('\n')
+
+
+# transform right skewed data inputted from the dataframe
+def transform_views_data(data_per_year):
+    data_per_year['views'] = np.sqrt(data_per_year['views'])
+    return(data_per_year)
+
+
+# Plot a histogram for 2018 and 2020 total views
+def plot_total_views_hist(total_data, country):
+
+    # Create plot axis
+    fig, ax = plt.subplots(figsize=(8,7))
+    for label, df in total_data.groupby('year'):
+        df.views.plot(kind="hist", ax=ax, label=label)
+        
+    plt.legend()
+
+    # Create the title of the barplot
+    title = str(country) + ' - Histogram of Total Views per Trending Videos for 2018 and 2020'
+
+    # Save file to local workiing directory
+    file_title = title + '.png'
+    plt.savefig(file_title, dpi=100)    
+
+
 ########################### MAIN ####################################
 def main(country):
 
@@ -197,8 +262,12 @@ def main(country):
         sys.stdout.write('\r')
         sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
         sys.stdout.flush()
-        sleep(0.1)    
+        sleep(0.1)   
+
+
+    ### RETRIEVING + TRANSFORMING THE DATA ###
     
+
     # Get the 2018 data for the input country
     vids_2018_dir = './2018 Data/' + str(country) + 'videos' + '.csv.zip'
     cats_2018_dir = './2018 Data/' + str(country) + '_category_id.json'
@@ -219,7 +288,11 @@ def main(country):
     create_barplot(all_data_merged, country)
 
     # print the dataframe
-    print("\n-> The Trending Count per Category for 2018 and 2020:\n\n", all_data_merged, '\n')
+    print("\n\n-> The Trending Count per Category for 2018 and 2020:\n\n", all_data_merged, '\n')
+
+
+    ### ANALYSIS: CHI-SQUARED TEST ###
+
 
     # Create the contingency table
     # columns -> 2018, 2020
@@ -230,8 +303,8 @@ def main(country):
 
     # Contingency table used in Chi-Squared Analysis:
     print('\n-> Contingency table used in Chi-Squared Analysis:')
-    print('\n2018 data:\n', col1, '\n')
-    print('\n2020 data:\n', col2, '\n')
+    print('\n2018 data:\n\n', col1, '\n')
+    print('\n2020 data:\n\n', col2, '\n')
 
     # Perform a Chi Square Test on the table
     chi2, p, dof, expected = stats.chi2_contingency(contingency_table)
@@ -239,6 +312,10 @@ def main(country):
     # Print results of the chi-squared test
     print('\n-> Chi Squared P-value for Wether or Not the Year had an Effect on the Category of Trending Videos:\n')
     print(p, '\n')
+
+
+    ### MORE DATA TRANSFORMATION ###
+
 
     # Get view data from trending categories
     views_per_cats_2018 = get_view_data_2018(vids_2018_dir, cats_2018_dir)
@@ -259,25 +336,70 @@ def main(country):
     views_per_cats_2018['cat_name'] = views_per_cats_2018['cat_name'].astype(str) + '_2018'
     views_per_cats_2020['cat_name'] = views_per_cats_2020['cat_name'].astype(str) + '_2020'
 
+
+    ### NORMALITY TEST + TRANSFORMATIONS ###
+
+
+    # Plot Normal Plot of views in each category
+    plot_views_hist(views_per_cats_2018, country, 2018, 0)
+    plot_views_hist(views_per_cats_2020, country, 2020, 0)
+
+    # Perform Normality Test (Uncomment to see results)
+    #print('\n-> Normality Test For Views per Category\n')
+    #normality_test_views(views_per_cats_2018, country, 2018)
+    #normality_test_views(views_per_cats_2020, country, 2020)
+
+    # transform the data
+    views_per_cats_2018_trans = transform_views_data(views_per_cats_2018)
+    views_per_cats_2020_trans = transform_views_data(views_per_cats_2020)
+
+    # Plot Normal Plot of views in each category
+    plot_views_hist(views_per_cats_2018_trans, country, 2018, 1)
+    plot_views_hist(views_per_cats_2020_trans, country, 2020, 1)
+
+    # UNCOMMENT HERE TO SEE FULL TEST
+    #print('\n-> Normality Test for Transformed Views per Category\n')
+    #normality_test_views(views_per_cats_2018_trans, country, 2018)
+    #normality_test_views(views_per_cats_2020_trans, country, 2020)
+
+
+    ### ANALYSIS: DIFFERENCE BETWEEN VIEWS PER YEAR ###
+
+
+    # append the data together
+    views_per_cats_2018['year'] = 2018
+    views_per_cats_2020['year'] = 2020
     all_labeled_views_per_cats = views_per_cats_2018.append(views_per_cats_2020, ignore_index=True)
 
-    # Tukey's Analysis
+    # Plot the total views for 2018 and 2020
+    plot_total_views_hist(all_labeled_views_per_cats, country)
+
+    # Perform a Mann-Whitney Test:
+    print('\n-> Mann-Whitney Test P-Value for Views of Trending Videos in 2018 and 2020')
+    mann_whit_test = stats.mannwhitneyu(views_per_cats_2018['views'], views_per_cats_2020['views'], alternative='two-sided')[1]
+    print('\n' + str(mann_whit_test) + '\n')
+
+
+    ### ANALYSIS: TUKEYS FOR CATEGORIES BETWEEN EACH YEAR ###
+
+    # Tukey's Analysis per category
     posthoc = pairwise_tukeyhsd(
             all_labeled_views_per_cats['views'], 
             all_labeled_views_per_cats['cat_name'],
             alpha=0.05)
 
-    print('\n-> PostHoc Analysis for Views Per Categories for 2018 vs 2020:\n')
-    print(posthoc)
+    # UNCOMMENT HERE TO SEE FULL POSTHOC
+    #print('\n-> PostHoc Analysis for Views Per Categories for 2018 vs 2020:\n')
+    #print(posthoc)
 
     # Save Tukey's data
     fig = posthoc.plot_simultaneous()    
     fig.set_size_inches(20, 20)
-    title = str(country) + '_Views_Per_Cats_Tukeys_Analysis.png'
+    title = str(country) + ' - Tukeys Test per Categories of 2018 and 2020.png'
     fig.savefig(title)
 
     # Print end line
-    print('\n\n\n  -> Analysis Complete <-  \n')
+    print('\n  -> Analysis Complete <-  \n')
 
 
 if __name__=='__main__':
